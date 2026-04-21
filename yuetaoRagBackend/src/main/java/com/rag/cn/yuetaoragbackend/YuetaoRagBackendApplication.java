f315b1a032f516f7611cd327c50ea95d37658d24
fantasy <384130598@qq.com>
2026-04-21 14:36:15 +0800

    init：yuetaoRag的后端模块初始化

---
+++ yuetaoRagBackend/src/main/java/com/rag/cn/yuetaoragbackend/YuetaoRagBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rag.cn.yuetaoragbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuetaoRagBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuetaoRagBackendApplication.class, args);
+    }
+
+}
---
+++ yuetaoRagBackend/src/test/java/com/rag/cn/yuetaoragbackend/YuetaoRagBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rag.cn.yuetaoragbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuetaoRagBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
