53dce623770e49f406b9094a9e4f087b5d1bb6ee
fantasy <384130598@qq.com>
2026-05-06 17:24:49 +0800

    feature：前端变更为React项目

--- yuetaoRagFront/vite.config.js
+++ yuetaoRagFront/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import path from "node:path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  },
+  server: {
+    port: 5174,
+    proxy: {
+      "/yuetaoRag": {
+        target: "http://localhost:9596",
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
+});
